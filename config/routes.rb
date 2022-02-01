5b99eb8319561da01b000499dd98bfd45c6d6d78
Dylan Harper <dylanharper4@icloud.com>
2022-01-31 20:59:39 -0800

    Finish basics for user story 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get '/welcome/index', to: 'welcome#index'
+
+  resources :user, only: [:create, :show] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
