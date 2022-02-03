03cfa6e35eb7042378ea7b242c3cd694633ab333
Joel Grant <joelmgrant@me.com>
2022-02-03 15:04:17 -0700

    Incorporate Movie search feature into API files

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -8,7 +8,7 @@ class MovieFacade
     movie_data = movie_data.flatten
 
     @_all_movies = movie_data.map do |data|
-
+      # require 'pry'; binding.pry
       MovieAPI.new(data.slice(:id,
                            :title,
                            :poster_path,
@@ -28,6 +28,18 @@ class MovieFacade
     top_20 = sorted.take(20)
   end
 
+  def self.search(query)
+    json = MovieService.movie_search(query)
+    results = json[:results].map do |movie|  
+    MovieAPI.new(movie.slice(:id,
+                         :title,
+                         :poster_path,
+                         :genre_ids,
+                         :overview,
+                         :vote_average,
+                         :vote_count))
+    end
+  end
 
   def service
     @_service ||= MovieService.new
