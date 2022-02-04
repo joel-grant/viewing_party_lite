c547057b1d5f239096a0a015dd0011184a3f12c2
Dylan Harper <dylanharper4@icloud.com>
2022-02-04 10:55:40 -0800

    Get movie name and duration to appear on viewing party create page

--- app/controllers/user_party_controller.rb
+++ app/controllers/user_party_controller.rb
@@ -1,6 +1,21 @@
 class UserPartyController < ApplicationController
+
   def new
     @movie = MovieFacade.movie_id_search(params[:movie_id])
-    # @genres = MovieGenreFacade.genres(@movie.genre_ids)
+    @user = User.find(params[:user_id])
+    @users = User.all
+  end
+
+  def create
+    party = Party.create!(duration: params[:duration].to_i,
+                  date: params[:select_date],
+                  start_time: params[:start_time],
+                  movie_id: params[:movie_id])
+
+    UserParty.create!(user_id: params[:user_id],
+                       party_id: party.id,
+                       status: "Host",)
+
+    redirect_to "/users/#{params[:user_id]}"
   end
 end
