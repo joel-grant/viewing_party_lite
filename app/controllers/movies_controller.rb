1f846df70a898be699f179991fa86544d90f729c
Joel Grant <joelmgrant@me.com>
2022-02-04 12:37:51 -0700

    Debug movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,11 @@
 class MoviesController < ApplicationController
   def results
+    # require 'pry'; binding.pry
     @user = User.find(params[:id])
 
     if params[:q] == 'top20'
       @results = MovieFacade.top_20
-    elsif params[:keyword]
+    else
       @results = MovieFacade.search(params[:keyword])
     end
   end
@@ -15,5 +16,6 @@ class MoviesController < ApplicationController
     @genres = MovieGenreFacade.genres(@movie.genre_ids)
     @cast = MovieCastFacade.first_10_cast_members(params[:movie_id])
     @reviews = MovieReviewFacade.reviews(params[:movie_id])
+
   end
 end
