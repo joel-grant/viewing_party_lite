ef788014a6ffc7c89ead25d2be9d7fab8e856702
Joel Grant <joelmgrant@me.com>
2022-02-03 17:43:28 -0700

    Change route to user_party

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   get '/users/:id/discover', to: 'users#discover'
   get '/users/:user_id/movies/:movie_id', to: 'movies#show'
-  get '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'movies#new' 
+  get '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'user_party#new' 
   get '/users/:id/movies', to: 'movies#results'
   # resource :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
