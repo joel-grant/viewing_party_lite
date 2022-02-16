3298040203ea02de42d220547537def899458a17
Joel Grant <joelmgrant@me.com>
2022-02-16 13:21:57 -0700

    Add fields for password and password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(name: params[:name], email: params[:email])
+    require 'pry'; binding.pry
+    @user = User.create(user_params)
     redirect_to "/users/#{@user.id}"
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 
 end
